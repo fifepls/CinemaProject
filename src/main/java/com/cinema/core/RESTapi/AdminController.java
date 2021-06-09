7e4fdde590e55bd3f32ae7fa9fe3fc4d2072f7dc
Konstantin Bezborodov <bezborodovk@mail.ru>
2021-06-09 19:53:30 +0300

    added admin rest controller

---
+++ src/main/java/com/cinema/core/RESTapi/AdminController.java
@@ -0,0 +1,10 @@
+package com.cinema.core.RESTapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+//дописать преауторайзы для методов
+public class AdminController {
+}
