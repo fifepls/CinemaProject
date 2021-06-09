ead6f553f9d06cd1a381f6cf8fc8b80c57749d08
Konstantin Bezborodov <bezborodovk@mail.ru>
2021-06-09 19:51:54 +0300

    added session repository

---
+++ src/main/java/com/cinema/core/repository/SessionRepository.java
@@ -0,0 +1,11 @@
+package com.cinema.core.repository;
+
+import com.cinema.core.entity.Film;
+import com.cinema.core.entity.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
+    public List<Session> findAllByFilmId(Long filmId);
+}
