3ead81ec19d8f26999675d9ba0697050618b814f
Konstantin Bezborodov <bezborodovk@mail.ru>
2021-06-09 19:06:15 +0300

    added news page

--- src/main/java/com/cinema/core/service/FilmService.java
+++ src/main/java/com/cinema/core/service/FilmService.java
@@ -4,8 +4,6 @@ import com.cinema.core.entity.Film;
 import com.cinema.core.repository.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
