b456a9d1fe30c93485c399e30220fe3cf90284b0
Konstantin Bezborodov <bezborodovk@mail.ru>
2021-06-09 19:52:51 +0300

    added news, login, home mapping

--- src/main/java/com/cinema/core/controller/MainPageController.java
+++ src/main/java/com/cinema/core/controller/MainPageController.java
@@ -1,7 +1,5 @@
 package com.cinema.core.controller;
 
-import com.cinema.core.service.FilmService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
